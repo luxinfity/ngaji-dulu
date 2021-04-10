945ab84fe08865445e66ffa6e649c05d35ebf31c
alifgiant <alif.akbar@bukalapak.com>
2021-04-10 14:25:07 +0800

    enable admin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,9 @@
 import * as functions from "firebase-functions";
 
 // The Firebase Admin SDK to access Firestore.
-// import * as admin from 'firebase-admin';
-// admin.initializeApp();
+import * as admin from 'firebase-admin';
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
