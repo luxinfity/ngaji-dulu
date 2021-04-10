449f47672e3b71507c0fe632e0af91675b3c14b7
alifgiant <alif.akbar@bukalapak.com>
2021-04-10 20:35:54 +0800

    add webhook function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,19 @@
-import * as functions from "firebase-functions";
-
 // The Firebase Admin SDK to access Firestore.
 import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
 admin.initializeApp();
 
 // Check for POST request, since telegram will only sent a post req
 // so no other source will be handled
 // https://core.telegram.org/bots/api#recent-changes
 export const webHook = functions.https.onRequest((request, response) => {
-  if (request.method !== "POST") {
-    response.status(400).send("Please send a POST request");
-    return;
-  }
-  functions.logger.info("Hello logs!", {structuredData: true, update: request.body});
-  response.send("WebHook accepted!");
+    if (request.method !== "POST") {
+        response.status(400).send("Please send a POST request");
+        return;
+    }
+    functions.logger.info(
+        "Hello logs!", { structuredData: true, update: request.body }
+    );
+    response.send("WebHook accepted!");
 });
