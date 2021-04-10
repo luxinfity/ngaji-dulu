89e6faff36ef0b17c1da8cf42328b1b74fa8a282
alifgiant <alif.akbar@bukalapak.com>
2021-04-10 19:33:10 +0800

    add webhook function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ngaji-dulu
 Ngaji-Dulu! a telegram bot to gamified users hobbit of reading Quran
+
+## Cloud Function (Server)
+read readme.md on /functions
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,17 @@
 import * as functions from "firebase-functions";
 
 // The Firebase Admin SDK to access Firestore.
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
 admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// Check for POST request, since telegram will only sent a post req
+// so no other source will be handled
+// https://core.telegram.org/bots/api#recent-changes
+export const webHook = functions.https.onRequest((request, response) => {
+  if (request.method !== "POST") {
+    response.status(400).send("Please send a POST request");
+    return;
+  }
+  functions.logger.info("Hello logs!", {structuredData: true, update: request.body});
+  response.send("WebHook accepted!");
 });
