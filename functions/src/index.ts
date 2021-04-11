50f45d70e73e14307c2b36476fa399ac0bfbbf54
alifgiant <alif.akbar@bukalapak.com>
2021-04-11 08:36:42 +0800

    create text reply

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 // The Firebase Admin SDK to access Firestore.
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import {Context, Telegraf} from "telegraf";
+import { Context, Telegraf } from "telegraf";
 import * as dotenv from "dotenv";
+import * as strings from "./strings";
 
 dotenv.config();
 
@@ -16,46 +17,56 @@ interface MyContext extends Context {
 const token = process.env.TELEGRAM_TOKEN || functions.config().telegram.token;
 const bot = new Telegraf<MyContext>(
     token,
-    {telegram: {webhookReply: true}}
+    { telegram: { webhookReply: true } }
 );
 
 // Register middleware and launch your bot as usual
 bot.use((ctx, next) => {
-  ctx.firstName = ctx.from?.first_name;
-  ctx.userId = ctx.from?.id;
-  return next();
+    ctx.firstName = ctx.from?.first_name;
+    ctx.userId = ctx.from?.id;
+    return next();
 });
 
 // error handling
 bot.catch((err, ctx) => {
-  functions.logger.error("[Bot] Error", err);
-  ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`);
-  return;
+    functions.logger.error("[Bot] Error", err);
+    ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`);
+    return;
 });
 
-// initialize the commands
-bot.command(
-    "/start",
-    (ctx) => ctx.reply("Hello! Send any message and I will copy it.")
-);
+// Listen for basic command
+bot.command("/start", (ctx) => ctx.reply(strings.startReply));
+bot.command("/help", (ctx) => ctx.reply(strings.startReply));
+bot.command("/read", (ctx) => ctx.reply(strings.readReply));
+bot.command("/profile", (ctx) => ctx.reply(strings.profileReply));
+bot.command("/reset", (ctx) => ctx.reply(strings.resetReply));
+bot.command("/quit", (ctx) => {
+    ctx.reply(strings.quitReply);
+    ctx.leaveChat();
+});
+
+// receive message with type text
+bot.on("text", (ctx) => ctx.reply("recieve message text"));
+
+// receive message with type text
+bot.on("voice", (ctx) => ctx.reply("recieve voice message"));
 
 // copy every message and send to the user
 bot.on("message", (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message));
-
 
 // Check for POST request, since telegram will only sent a post req
 // so no other source will be handled
 // https://core.telegram.org/bots/api#recent-changes
 export const webHook = functions.https.onRequest((request, response) => {
-  if (request.method !== "POST") {
-    response.status(400).send("Please send a POST request");
-    return;
-  }
+    if (request.method !== "POST") {
+        response.status(400).send("Please send a POST request");
+        return;
+    }
 
-  functions.logger.info(
-      "Incoming message", {data: request.body}
-  );
+    functions.logger.info(
+        "Incoming message", { data: request.body }
+    );
 
-  bot.handleUpdate(request.body, response);
-  response.status(200).send("WebHook accepted!");
+    bot.handleUpdate(request.body, response);
+    response.status(200).send("WebHook accepted!");
 });
---
+++ functions/src/strings.ts
@@ -0,0 +1,26 @@
+export const startReply = `
+Bot ini berfungsi untuk membantu kamu,
+melakukan pengukuran terhadap kebiasaan tilawah mu.
+Guna meningkatkan kebiasaan positif bertilawah selama
+bulan Ramadan ini. Untuk tutorial penggunaan silahkan lihat di [sini](https://luxinfity.github.io/ngaji-dulu/).
+
+Untuk memunculkan kembali pesan ini ketikkan /help.
+Untuk mulai mengaji ketikkan /read.
+Untuk melihat progress kamu ini ketikkan /profile.
+Untuk merestart progress kamu ini ketikkan /reset.
+Untuk berhenti menggunakan bot /quit.
+
+Jika ada saran dan masukan silahkan (chat)[t.me/alifgiant] admin kami
+`;
+
+export const readReply = `mulai mengai`;
+
+export const profileReply = `
+Kamu sudah menyelesaikan membaca quran xx%
+Minggu ini kamu mengaji total x jam nn menit.
+Silahkan klik [disini](https://google.com) untuk melihat lebih banyak statistik.
+`;
+
+export const resetReply = `progress mu telah di reset`;
+
+export const quitReply = `Semoga amal ibadah diterima disisinya, sampai berjumpa lagi`;
