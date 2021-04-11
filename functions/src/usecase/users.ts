071bcfc7a38827e6856ac4c7892a44e71dd93be2
alifgiant <alif.akbar@bukalapak.com>
2021-04-11 12:30:12 +0800

    add firestore to emulator and save user data to firestore

--- functions/src/usecase/users.ts
+++ functions/src/usecase/users.ts
@@ -2,13 +2,19 @@ import * as admin from "firebase-admin";
 import { BotContext } from "../core/bot-context";
 import * as strings from "../core/strings";
 
-export function addNewUser(ctx: BotContext) {
+export async function addNewUser(ctx: BotContext) {
     ctx.replyWithMarkdownV2(strings.startReply);
     if (!ctx.userId || !ctx.from) return;
 
     const db = admin.firestore();
-    const collection = db.collection("users");
-    collection.doc(ctx.userId).set(ctx.from);
+    const doc = db.collection("users").doc(ctx.userId);
+    const snapshot = await doc.get();
+    if (!snapshot.exists) {
+        doc.set(ctx.from);
+        db.collection("statistic").doc("users").update({
+            "total": admin.firestore.FieldValue.increment(1)
+        });
+    }
 }
 
 // function getUserStatus() {
